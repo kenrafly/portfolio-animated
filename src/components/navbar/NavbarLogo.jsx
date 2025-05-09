649ed56491de9e6ea8151bdf94e1a5c3fb5bb059
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2025-05-09 15:30:53 +0700

    first commit

---
+++ src/components/navbar/NavbarLogo.jsx
@@ -0,0 +1,14 @@
+const NavbarLogo = () => {
+  return (
+    <div>
+      <h1 className="text-white  text-2xl sm:hidden md:block">
+        Rafly Ardiansyah
+      </h1>
+      <h1 className="text-white font-special font-extrabold text-4xl md:hidden sm:block">
+        RA
+      </h1>
+    </div>
+  );
+};
+
+export default NavbarLogo;
