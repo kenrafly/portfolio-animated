8f9bd5ae5fabb9924f52796a93ed8f6d7520201e
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2025-05-09 16:22:05 +0700

    third

--- src/components/heroSection/HeroPic.jsx
+++ src/components/heroSection/HeroPic.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 import { PiHexagonThin } from "react-icons/pi";
-import HeroPicBg from "../../public/images/HexaPic.png";
+import HeroPicBg from "../../../public/images/HexaPic.png";
 
 const HeroPic = () => {
   return (
